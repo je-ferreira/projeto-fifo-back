efe41170d31bea5c8c40a1e4a9623d9d97c2104e
Raphael Fleury <raphael.fleury9@gmail.com>
2020-11-08 01:56:27 -0300

    feat: Controller pronto

--- src/main/java/com/squad5/fifo/controller/DispositivoController.java
+++ src/main/java/com/squad5/fifo/controller/DispositivoController.java
@@ -1,9 +1,19 @@
 package com.squad5.fifo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.squad5.fifo.dto.DispositivoDTO;
 import com.squad5.fifo.service.DispositivoService;
 
 @RestController @RequestMapping("/dispositivos")
@@ -11,4 +21,29 @@ public class DispositivoController {
 
 	@Autowired
 	private DispositivoService service;
+	
+	@GetMapping("/{id}")
+	public DispositivoDTO get(@PathVariable long id) {
+		return service.findById(id);				
+	}
+	
+	@GetMapping
+	public List<DispositivoDTO> getAll() {
+		return service.findAll();				
+	}
+	
+	@PostMapping
+	public DispositivoDTO post(@RequestBody @Validated DispositivoDTO dto) {
+		return service.insert(dto);				
+	}
+	
+	@PutMapping
+	public DispositivoDTO put(@RequestBody DispositivoDTO dto) {
+		return service.update(dto);				
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		service.deleteById(id);			
+	}
 }
--- src/main/java/com/squad5/fifo/service/DispositivoService.java
+++ src/main/java/com/squad5/fifo/service/DispositivoService.java
@@ -1,8 +1,12 @@
 package com.squad5.fifo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.squad5.fifo.dto.DispositivoDTO;
 import com.squad5.fifo.model.Dispositivo;
 import com.squad5.fifo.repository.DispositivoRepository;
 
@@ -11,4 +15,24 @@ public class DispositivoService {
 	
 	@Autowired
 	private DispositivoRepository repository;
+	
+	public DispositivoDTO findById(long id) {
+		return null;
+	}
+	
+	public List<DispositivoDTO> findAll() {
+		return new ArrayList<DispositivoDTO>();
+	}
+	
+	public DispositivoDTO insert(DispositivoDTO dto) {
+		return dto;
+	}
+	
+	public DispositivoDTO update(DispositivoDTO dto) {
+		return dto;
+	}
+	
+	public void deleteById(long id) {
+		return;
+	}
 }
