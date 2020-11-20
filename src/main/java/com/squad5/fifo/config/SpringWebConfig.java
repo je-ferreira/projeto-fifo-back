eb4db39cbd549320218d8c9e44dffb559debda48
Victor Matheus <victormatheusdonascimento@gmail.com>
2020-11-20 16:02:54 -0300

    fix: rota do swagger
    Correção do bloqueio da rota do swagger que ocorreu após a configuração do CORS.

--- src/main/java/com/squad5/fifo/config/SpringWebConfig.java
+++ src/main/java/com/squad5/fifo/config/SpringWebConfig.java
@@ -3,14 +3,26 @@ package com.squad5.fifo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration @EnableWebMvc
 public class SpringWebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
