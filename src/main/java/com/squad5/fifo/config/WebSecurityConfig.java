afc2a1adfbdcb59b438e0545fcd7c236c43661a9
dalmo <mffacuri@terra.com.br>
2020-11-28 13:53:46 -0200

    feat(config): dependências e configuração básica
    Foram  adicionadas  as dependências do Spring Boot Security e JWT  para a liberação de todas as rotas.

---
+++ src/main/java/com/squad5/fifo/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.squad5.fifo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity ( prePostEnabled = true )
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
+	
+	@Override
+	protected void configure ( HttpSecurity httpSecurity ) throws Exception {
+		httpSecurity.authorizeRequests()
+		.anyRequest().permitAll()
+		.and().csrf().disable();
+	}
+
+}
